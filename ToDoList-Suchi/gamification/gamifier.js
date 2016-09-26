f0eab4d16d1d274dbabce5a622ad3e250ff34279
Muhammad Abduh <muhammad.abduh.arifin@rwth-aachen.de>
2016-09-26 16:42:06 +0700

    update

---
+++ ToDoList-Suchi/gamification/gamifier.js
@@ -0,0 +1,110 @@
+﻿  var memberId,
+  appId = '$Application_Id$',
+  epURL = 'http://gaudi.informatik.rwth-aachen.de:8081/',
+  iwcGamification;
+
+
+  var useAuthentication = function(rurl){
+    if(rurl.indexOf("\?") > 0){
+      rurl += "&access_token=" + window.localStorage["access_token"];
+    } else {
+      rurl += "?access_token=" + window.localStorage["access_token"];
+    }
+    return rurl;
+  }
+
+/**
+ * Gamifier module
+ */
+  var Gamifier = {
+    triggerAction : function(actionId){
+      console.log("Action triggered : " + actionId);
+      $.post(
+       useAuthentication(epURL + 'visualization/actions/' + appId + '/' + actionId + '/' + memberId),
+       ''
+       ).done(function(data) {
+         console.log('Trigger success : ' + actionId);
+         console.log(data);
+         if (data.length > 0){
+             sendOpenNotificationIntent(JSON.stringify(data));
+         }
+         sendRefreshTabIntent();
+       })
+       .fail(function() {
+         alert( "Trigger failed " + actionId );
+       });
+    }
+  };
+
+/**
+ * Initialize gamification
+ */
+  var initGamification = function() {
+
+    iwcGamification = new iwc.Client();
+    iwcGamification.connect(
+      function(intent) {
+        // define your reactions on incoming iwc events here
+
+        if(intent.action == "FETCH_APPID") {
+          sendRefreshAppIdIntent();
+        }
+
+    });
+    sendRefreshAppIdIntent();
+  };
+
+  function sendRefreshTabIntent(){
+    var intent = {
+      "component": "",
+      "data": "",
+      "dataType": "text/xml",
+      "action": "REFRESH_TAB",
+      "categories": ["", ""],
+      "flags": [void 0],
+      "extras": {}
+    };
+    iwcGamification.publish(intent);
+  }
+
+  function sendRefreshAppIdIntent(){
+    var data = appId;
+    var intent = {
+      "component": "",
+      "data": data,
+      "dataType": "text/xml",
+      "action": "REFRESH_APPID",
+      "categories": ["", ""],
+      "flags": [void 0],
+      "extras": {}
+    };
+    iwcGamification.publish(intent);
+  }
+
+  function sendOpenNotificationIntent(data){
+	    var intent = {
+	      "component": "",
+	      "data": data,
+	      "dataType": "text/xml",
+	      "action": "OPEN_NOTIFICATION",
+	      "categories": ["", ""],
+	      "flags": [void 0],
+	      "extras": {}
+	    };
+	    iwcGamification.publish(intent);
+  }
+  function signInCallback(result) {
+    if (result === 'success') {
+      memberId = oidc_userinfo.preferred_username;
+      console.log(oidc_userinfo);
+      console.log('Logged in!');
+      initGamification();
+    } else {
+      console.log(result);
+      console.log(window.localStorage['access_token']);
+      alert("Failed to log in using Open ID connect. Cannot use gamification feature.");
+    }
+  }
+
+  $(document).ready(function() {
+  });
